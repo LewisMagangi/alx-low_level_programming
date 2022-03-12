3c914c16ce6fb4090ab68b803352278cc6a3bab7
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-11 16:33:41 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(void)
+{
+int a;
+char c;
+for (a = '1'; a <= '9'; a++)
+putchar(a);
+for (c = 'a'; c <= 'g'; c++)
+{
+if (c < 'g')
+{
+putchar(c);
+}
+else if (c == 'g')
+{
+putchar('\n');
+}
+}
+return (0);
+}

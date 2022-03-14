86f62ac794f26f2f74a001111ee007868eec8db2
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-14 04:26:19 -0700

    Betty Documentation Style Update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
+/**
+ * main -Entry Point
+ *
+ * Return :Always 0
+ */
 int main(void)
 {
 int n;

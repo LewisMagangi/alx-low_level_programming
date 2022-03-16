b840007c3f57a6aa556bdd0daadb186052b0138b
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 16:58:26 -0700

    Pushing times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table - a function that prints the 9 times table
+ * times_table - a function that prints the 9 times table starting with 0
  * Return: times table
  */
 void times_table(void)
@@ -18,20 +18,20 @@ for (row = 0; row <= 9; row++)
 
 		if ((i / 10) > 0)
 		{
-		_putchar((i / 10) + '0');
+			_putchar ((i / 10) + '0');
 		}
 		else
 		{
-		_putchar(' ');
+			_putchar (' ');
 		}
-		_putchar((i % 10) + '0');
+			_putchar ((i % 10) + '0');
 
 		if (col < 9)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar (',');
+			_putchar (' ');
 		}
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
 

fb70d9aae43602a67b718d5b1b2c6aa58bba4a80
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 15:27:55 -0700

    Adding print_sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		putchar ('+');
 		return (1);
 	}
 	else if (n == 0);
 	{
-		putchar('0');
+		putchar ('0');
 		return (1);
 	}
 	else
 	{
-		putchar('-');
+		putchar ('-');
 		return (-1);
 	}
 }

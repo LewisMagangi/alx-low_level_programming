12d2ecbaf56939754f747237526f0f8e2e8f1a1c
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 13:22:36 -0700

    Adding is_lower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int _islower(int c);
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+int c;
+
+if (c => 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+

0486fe5bc56f70bfc0f895139a72ac40c9f9b214
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 15:04:25 -0700

    Pushing 5-sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,19 @@
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-putchar('+');
-return (1);
-}
-else if (n == 0);
-{
-putchar('0');
-return (1);
-}
-else 
-{
-putchar('-');
-return (-1);
-}
+	if (n > 0)
+	{
+	putchar('+');
+	return (1);
+	}
+	else if (n == 0);
+	{
+	putchar('0');
+	return (1);
+	}
+	else 
+	{
+	putchar('-');
+	return (-1);
+	}
 }

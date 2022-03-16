eab03a3f0ff5ecffa5a179bd77096f747bf4f473
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 14:34:27 -0700

    Adding isalpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,12 @@
 #include "main.h"
-int _isalpha(int c);
-
+/**
+ * _isalpha - a function that checks if c is a letter
+ *
+ * @c: single letter input
+ * Returns 1 if c is a letter else returns 1
+ */
 int _isalpha(int c)
 {
-int c;
-
 if (c >= '65' && c <= '90' || c >= '97' && c <= '122')
 {
 return (1);

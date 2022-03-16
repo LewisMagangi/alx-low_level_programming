257b0620bb8b9a812980286ed5583b7528efd497
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 15:02:14 -0700

    Pushing 5-sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * print_sign - a function that prints the sign of a number
+ * @n: number input
+ * Return: 1 if n > 0; 0 if n === 0; -1 if n < 0
+ */
 int print_sign(int n)
 {
 if (n > 0)

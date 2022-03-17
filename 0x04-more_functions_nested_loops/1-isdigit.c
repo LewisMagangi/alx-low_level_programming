0a8a8a24150535a3c63d30c415d21b607bfbc414
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-17 12:04:28 -0700

    Fixing is digit function

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,11 @@
 /**
  * _isdigit - a function that checks for a digit
  * @c: single letter input
- * Returns: 1 if c is a digit, 0 if otherwise
+ * Return: 1 if c is a digit, 0 if otherwise
  */
 int _isdigit(int c)
 {
-	if ((c >= '48') && (c <= '57')
+	if ((c >= '48') && (c <= '57'))
 	{
 		return (1);
 	}

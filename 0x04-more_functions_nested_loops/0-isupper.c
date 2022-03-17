20d83ede3851569c87d7dce15db3cf42fc48fe68
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-17 06:18:34 -0700

    Adding isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks if c is uppercase
+ * @c: single letter input
+ * Returns: 1 if c is uppercase, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

a2f3fa2baff2a0fe9baa3879044fb87d66472b77
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-17 13:48:52 -0700

    Hot Fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,9 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = '0'; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		if (n <= '0')
+		if (n <= 0)
 		{
 			putchar ('\n');
 		}
@@ -20,7 +20,7 @@ void print_diagonal(int n)
 		}
 		else
 		{
-			for (j = '1'; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				if (j < i)
 				{

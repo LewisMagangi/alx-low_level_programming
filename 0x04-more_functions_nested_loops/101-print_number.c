4ae0b032e9d4275a5bf9ddd5e5640c0c9967f1e5
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-17 15:43:42 -0700

    Hot Fix

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	_putchar(n + '0');
+	return (0);
+}

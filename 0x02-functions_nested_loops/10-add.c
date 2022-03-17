11180f1c28cf7374449796ea49146f4297829e76
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 17:06:15 -0700

    Pushing add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two numbers
+ * @one: first number input
+ * @two: second number input
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}

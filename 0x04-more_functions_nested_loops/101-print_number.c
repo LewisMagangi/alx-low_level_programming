95fdba0a22f8bce5a815ce8cf123dc7a591fc929
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-17 15:53:37 -0700

    Pushing print_number function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_number - a function that prints an integer
+ *@n: an interger input
  */
-int main(void)
+void print_number(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	_putchar(n + '0');
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }

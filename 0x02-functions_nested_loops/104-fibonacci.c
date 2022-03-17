bf112a5c2f8b57413276685ca4d456198a951d35
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 19:36:03 -0700

    Pushing 104-fibonacci function

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int i, t1, t2, next;
+
+	t1 = 1;
+	t2 = 2;
+	next = t1 + t2;
+
+	printf("%li, %li, ", t1, t2);
+
+	for (i = 3; i <= 98; ++i)
+	{
+		printf("%li, ", next);
+		t1 = t2;
+		t2 = next;
+		next = t1 + t2
+			;
+	}
+	printf("%li", next);
+	printf("\n");
+	return (0);
+}

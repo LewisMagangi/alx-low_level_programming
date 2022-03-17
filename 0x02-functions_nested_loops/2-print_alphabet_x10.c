4ae0a9f80947c1cc4c0b4f9ae85f7960455ebebf
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-16 19:53:13 -0700

    Pushing 1-alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-char a;
-int b;
-b = 0;
+	int a, b;
+	b = 0;
 	while (b < 10)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
-		_putchar(a);
+			_putchar(a);
 		}
-	b++;
+		b++;
 	_putchar('\n');
 	}
 }

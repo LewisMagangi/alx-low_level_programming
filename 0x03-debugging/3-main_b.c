965572312af4aa057c5ea45117867909660a258d
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-18 15:34:22 -0700

    Hot Fix

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - takes a date and prints how many days are left in the year, taking 
+ * main - takes a date and prints how many days are left in the year, taking
  * leap years into account
  * Return: 0
  */
@@ -22,5 +22,3 @@ int main(void)
 	print_remaining_days(month, day, year);
 	return (0);
 }
-
-

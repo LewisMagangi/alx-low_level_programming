3dcc5444cca014eeea6a7f7616e450f7e3fffebc
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-18 13:29:29 -0700

    Adding a function

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	while (i < 10)/* Code causing an  infinite loop*/
@@ -16,6 +15,5 @@ int main(void)
 		putchar(i);
 	}
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }

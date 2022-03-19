6ada602af4985a5c2e15426b8219ef51fc8c5f46
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-19 04:54:21 -0700

    Fixing a syntax error in the print_most numbers function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,14 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		if (a == 2)
+		if (a == '2')
 			a++;
-		else if (a == 4)
+		else if (a == '4')
 			a++;
 		else
-			_putchar (a + '0');
+			_putchar (a);
 	}
 	_putchar ('\n');
 }

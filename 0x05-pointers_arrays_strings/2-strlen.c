dcc6c600878ed6593e134883cc36bc7d4b73cc1f
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-21 14:40:53 -0700

    Pushing _strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: a character input
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	for (char i = *s; i != '\0'; i++)
+	{
+		_strlen = 0;
+
+		_strlen++;
+	}
+}

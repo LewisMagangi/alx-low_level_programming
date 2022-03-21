9ef3ca9059cc69ac4425a706bcf5529b779b781f
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-21 13:45:42 -0700

    pushing swap_int function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that swaps values of 2 intergers
+ * @a: interger input
+ * @b: 2nd interger input
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}

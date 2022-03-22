d6de478e0903cc5843724577931ea0c6db8d107e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-21 19:06:51 -0700

    Pushing puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,16 +5,22 @@
  */
 void puts_half(char *str)
 {
-	int i, x, n;
+	int i, n;
 
-	if (n % 2 == 0)
-		n = _strlen(str) / 2;
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+		n = i / 2;
 	else
-		n = ((_strlen(str) - 1) / 2);
-
+		n = (i - 1) / 2;
 	while (*(str + n) != '\0')
 	{
 		_putchar(*(str + n));
+		n++;
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }

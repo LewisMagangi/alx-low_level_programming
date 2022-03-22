be9b6f9c0bad52d0c826d27e425208bd579a3f96
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-21 18:32:00 -0700

    Fixing print_reverse

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - a function that prints every other character of a string
+ * @str: A character input
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+	_putchar('\n');
+}

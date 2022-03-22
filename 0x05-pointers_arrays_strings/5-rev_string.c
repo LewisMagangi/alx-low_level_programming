4cac693eecf9801b40303539cae6b3538cf9f4cd
Liquelaliqour <lewismomanyi34@gmail.com>
2022-03-21 18:08:50 -0700

    Fixing print_reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string
+ * @s: A character input
+ */
+void rev_string(char *s)
+{
+	int j = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j++;
+	}
+	for (n = j - 1; n >= 0; n--)
+	{
+		char *s = s[n];
+	}
+}

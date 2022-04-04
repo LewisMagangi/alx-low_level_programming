f0e81700e6431ea4c77b1ee35e9d066b2ff783e9
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-04 03:19:55 -0700

    Logical Error update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		if (a == '2' || a == '4')
+		if (a == 2 || a == 4)
 			a++;
 		else
 			_putchar (a);

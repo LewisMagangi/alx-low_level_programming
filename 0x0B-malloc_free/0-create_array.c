d1580792fd54d1975232a978f7b3dad62bc9c9af
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-05 09:23:35 -0700

    Pushing a function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *create_array - a function that creates an array of characters
+ * @c: character input
+ * Return: NULL if size = 0
+ * Return: A pointer to the array or NULL if it fails
+ */
+char *create_array(assigned int size, char c)
+{
+	char c[] = 'c';
+	
+	return(char *c);
+	
+	else if (size of(c) == 0)
+		return(NULL);
+	else 
+		return(NULL);
+}	

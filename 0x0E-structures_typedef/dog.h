afa9f4e168dc7b02eb22f1a61d32ee1db3fa370c
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-11 12:17:45 -0700

    pushing a dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a data structure for dogs
+ * @name: character *input
+ * @age: float input
+ * @owner: character *input
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */

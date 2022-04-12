d5b7d50d244b8331275d365ae88e3e106c469354
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-12 15:05:31 -0700

    new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,11 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 #endif /* DOG_H */

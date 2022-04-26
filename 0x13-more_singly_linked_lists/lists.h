01460d65a3f47401610b2f1f4b52ddfb4bd7735b
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-26 06:16:31 -0700

    Done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,26 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);

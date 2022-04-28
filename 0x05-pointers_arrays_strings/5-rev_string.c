6ec19f8daff30074499b49c8ae803b2f34319f1e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-28 06:00:21 -0700

    Fixing an Error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
-  *rev_string - A function that prints a string, in reverse, followed by a new line.
-  *@s - A character input
-  *
-  *Return: void
-  */
+ *rev_string -A function that reverses a string.
+ *@s: A character input
+ *
+ *Return: void
+ */
 void rev_string(char *s)
 {
-	int x = 0, y = 0, len = 0;
-	char u[];
+	int x = 0, len = 0;
+	char temp;
+
 	while (*(s + len) != '\0')
 	{
 		len++;
 	}
-	y = len;
-
-	while (0 < y)
+	while (x < len)
 	{
-		u[x] = s[len - 1];
-		x++;
 		len--;
+		temp = s[x];
+		s[x] = s[len];
+		s[len] = temp;
+		x++;
 	}
-	*s = *u;
 }
 

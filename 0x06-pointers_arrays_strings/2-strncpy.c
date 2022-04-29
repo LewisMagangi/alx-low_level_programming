85559181e4f88feae89dc3e086b035850fa1dd8b
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-29 06:42:42 -0700

    Fixing an Error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
   *_strncpy- a function that copies a string.
   *@dest: A character to be copied to
@@ -12,10 +12,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i != n && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	
+	while (i < n)
+	        dest[i++] = '\0';
+	
 	return (dest);
 }

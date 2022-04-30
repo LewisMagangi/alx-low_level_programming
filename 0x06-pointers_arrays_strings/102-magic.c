f814e87a9c0682f9546843b9cb833915095e09f4
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-30 01:05:00 -0700

    Adding a line of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-/**
-  *main-Entry Point
-  *@void: Nothing
-  *
-  *Return: An interger
-  */
+
 int main(void)
 {
 	int n;
@@ -14,8 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 *
-	*write your line of code here...
+	 *write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p

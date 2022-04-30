da608c2d37c48ce5243bf7538b13c2988c1db0ac
Liquelaliqour <lewismomanyi34@gmail.com>
2022-04-29 23:41:01 -0700

    FIXING A LOGICAL ERROR

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
   *rot13-a function that encodes a string using rot13.
   *@n: A character input
@@ -11,18 +10,20 @@ char *rot13(char *n)
 	int i = 0;
 	char input[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char output[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int j = 0;
 
 	while (n[i])
 	{
-		if (((n[i] >= 'a') && (n[i] <= 'z')) || ((n[i] >= 'A') && (n[i] <= 'Z')))
+		while (j < 52)
 		{
-			while (input[i])
+			if (n[i] == input[j])
 			{
-				output[i] = input[i];
-				i++;
+				n[i] = output[j];
+				break;
 			}
+			j++;
 		}
+		i++;
 	}
-	n = output;
 	return (n);
 }

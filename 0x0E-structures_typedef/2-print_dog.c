ce667fbfe3ff2bac2137a9c7dcc320f517943a57
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-03 03:16:24 -0700

    Updating print dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 /**
   *print_dog-a function that prints a struct dog
   *@d: A dog's data structure
@@ -8,5 +9,19 @@
   */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0.0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,6 @@ struct dog
 };
 
 typedef struct dog dog_t;
-
+void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */

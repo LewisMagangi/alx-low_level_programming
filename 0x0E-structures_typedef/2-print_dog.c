fa23e8f9095f93dcfb6f5664475834c0a8862cc5
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-03 02:20:27 -0700

    Ok

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include<stdio.h>
+/**
+  *print_dog-a function that prints a struct dog
+  *@d: A dog's data structure
+  *
+  *Return: void
+  */
+void print_dog(struct dog *d)
+{
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,8 @@ struct dog
 	char *owner;
 	float age;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */

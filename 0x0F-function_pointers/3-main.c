f03f0122ae557e9b8330f2a1753c4b18be73b8b2
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-03 22:39:14 -0700

    A goal ..., something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -9,4 +11,22 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}

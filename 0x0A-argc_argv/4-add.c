5112bec641473651cc35f55e345770c1f3eab9d0
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-11 02:55:49 -0700

    add no argument function

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-Entry Point
+ *@argc: Argument Count
+ *@argv: An array of strings
+ *
+ *Return: Interger
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}

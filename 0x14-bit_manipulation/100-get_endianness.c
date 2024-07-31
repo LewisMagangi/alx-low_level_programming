b4f48998679786e499e1b29500f9f01ad33a4690
Liquelaliqour <lewismomanyi34@gmail.com>
2024-07-31 07:57:56 +0000

    Style Fix.

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * get_endianness - Entry Point
-  * Return: 0
-  */
+* get_endianness - Entry Point
+* Return: 0
+*/
 int get_endianness(void)
 {
-		unsigned int i;
+	unsigned int i;
 
-			i = 1;
-				if (*(char *)&i == 1)
-							return (1);
-					else
-								return (0);
+	i = 1;
+	if (*(char *)&i == 1)
+		return (1);
+	else
+		return (0);
 }

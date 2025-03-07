492a316805ac0d1fb5515fe9b8f51219052582e1
Liquelaliqour <lewismomanyi34@gmail.com>
2025-03-07 13:18:45 +0300

    Fix: Add helper function prototype in 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,14 @@
 #include "main.h"
+
+/**
+ * helper - Recursive helper function to find the square root
+ * @n: The number to find the square root of
+ * @guess: The current guess for the square root
+ *
+ * Return: The natural square root of n, or -1 if n has no natural square root
+ */
+int helper(int n, int guess);
+
 /**
   *_sqrt_recursion-a function that returns the natural square root of a number.
   *@n: Interger input
@@ -6,9 +16,25 @@
   *Return: The squareroot of n
   */
 int _sqrt_recursion(int n)
+{ 
+  if (n < 0)
+    return (-1);
+  return (helper(n, 1));
+}
+
+
+/**
+ * helper - Recursive helper function to find the square root
+ * @n: The number to find the square root of
+ * @guess: The current guess for the square root
+ *
+ * Return: The natural square root of n, or -1 if n has no natural square root
+ */
+int helper(int n, int guess)
 {
-	if (n == 1)
-		return (1);
-	else 
-		return ( _sqrt_recursion(n ** 1/2));
+  if (guess * guess == n)
+    return (guess);
+  if (guess * guess > n)
+    return (-1);
+  return helper(n, guess + 1);
 }

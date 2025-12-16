9f3a987ae5e4bf25cca1abeb9edf4feb8c656931
Liquelaliqour <lewismomanyi34@gmail.com>
2025-12-16 16:15:53 +0300

    fix: use write syscall for stderr output to comply with task constraints

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }

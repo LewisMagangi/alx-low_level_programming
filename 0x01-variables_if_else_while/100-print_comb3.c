332739b08014af376a4954ce55c5e43ce10a7d96
Liquelaliqour <lewismomanyi34@gmail.com>
2025-12-16 16:26:23 +0300

    feat: implement program to print combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-int i;
-int j;
+	int i, j;
 
-for(i = 0; i++; i 
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

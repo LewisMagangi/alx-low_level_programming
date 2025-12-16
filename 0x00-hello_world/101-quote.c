b0a275d85d74aec9fbd397d11e94333e1f8136c5
Liquelaliqour <lewismomanyi34@gmail.com>
2025-12-16 14:04:42 +0300

    feat: implement C program to print Dora Korpar's quote to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints Dora's quote to stderr
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+    return (1);
+}

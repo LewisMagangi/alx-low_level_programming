27459491ebac53a3240d7395f49549deb8fcc8ff
Liquelaliqour <lewismomanyi34@gmail.com>
2025-12-16 17:47:05 +0300

    fix: correct Fibonacci count to print first 98 terms

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - prints the first 98 Fibonacci numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	unsigned long int i, t1, t2, next;
+	unsigned long int a1 = 1, b1 = 2;
+	unsigned long int a2 = 0, b2 = 0;
+	unsigned long int sum1, sum2;
+	unsigned long int limit = 1000000000;
+	int count;
 
-	t1 = 1;
-	t2 = 2;
-	next = t1 + t2;
+	printf("1, 2");
 
-	printf("%li, %li, ", t1, t2);
-
-	for (i = 3; i <= 98; ++i)
+	for (count = 3; count <= 98; count++)
 	{
-		printf("%li, ", next);
-		t1 = t2;
-		t2 = next;
-		next = t1 + t2
-			;
+		sum2 = a2 + b2;
+		sum1 = a1 + b1 + (sum2 >= limit);
+		sum2 %= limit;
+
+		printf(", ");
+		if (sum1)
+			printf("%lu%09lu", sum1, sum2);
+		else
+			printf("%lu", sum2);
+
+		a1 = b1;
+		a2 = b2;
+		b1 = sum1;
+		b2 = sum2;
 	}
-	printf("%li", next);
+
 	printf("\n");
 	return (0);
 }

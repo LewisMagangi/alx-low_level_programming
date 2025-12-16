8f97c7f30a67a3d94e8527a1662774552c48279d
Liquelaliqour <lewismomanyi34@gmail.com>
2025-12-16 17:17:12 +0300

    fix(style): Adhering to betty style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
+ * _isalpha - checks for an alphabetic character
+ * @c: character to check
  *
- * _isalpha - a function that checks for alphabetic character
- *
- * @c: single letter input
- *
- * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
- *
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-	return (1);
-else
-	return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
